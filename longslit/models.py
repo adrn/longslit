d5a5a4e308eb1f1926a0d4b0e9ee4c2230bd32ee
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-03-23 15:18:03 -0400

    oops voigt

--- longslit/models.py
+++ longslit/models.py
@@ -1,5 +1,6 @@
 # Third-party
 import numpy as np
+from scipy.special import wofz
 
 sqrt_2pi = np.sqrt(2*np.pi)
 def gaussian(x, amp, mu, sigma):
@@ -36,3 +37,25 @@ def gaussian_constant(x, amp, mu, sigma, offset):
     """
     return gaussian_polynomial(x, amp, mu, sigma, offset)
 
+def voigt(x, amp, x0, G_std, L_fwhm):
+    """
+    Voigt profile - convolution of a Gaussian and Lorentzian.
+
+    When G_std -> 0, the profile approaches a Lorentzian. When L_fwhm=0,
+    the profile is a Gaussian.
+
+    Parameters
+    ----------
+    x : numeric, array_like
+    amp : numeric
+        Amplitude of the profile (integral).
+    x0 : numeric
+        Centroid.
+    G_std : numeric
+        Standard of deviation of the Gaussian component.
+    L_fwhm : numeric
+        FWHM of the Lorentzian component.
+    """
+    _x = x-x0
+    z = (_x + 1j*L_fwhm/2.) / (np.sqrt(2.)*G_std)
+    return amp * wofz(z).real / (np.sqrt(2.*np.pi)*G_std)
